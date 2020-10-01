26325df351525d823fe425f82b1c30a02698c8ff
DESKTOP-REDASDA\admin <javigta97@gmail.com>
2020-10-01 10:57:01 +0200

    Segundo paso del tutorial

--- p1_ADC/p1_ADC.ino
+++ p1_ADC/p1_ADC.ino
@@ -3,7 +3,7 @@
  * -----------
  *Con una periodicidad de un segundo, lee el valor del ADC y muéstralo por consola. Utiliza la salida de
   3V3 de la placa con un potenciómetro para generar la tensión analógica. Antes de conectarla al
-  módulo comprueba que no superas 3V3 con un multímetro. 
+  módulo comprueba que no superas 3V3 con un multímetro --. 
  */
 
  const int ADC_pin = 36;
